27c88df062acb01f13592afe0b9aba5d17560465
Renata Furini <renatafurini@unochapeco.edu.br>
2024-02-09 17:28:47 -0300

    08022024-1

--- CalculoIMC.js
+++ CalculoIMC.js
@@ -1,7 +1,5 @@
-<script>
-
-let altura = 1.61;
-let peso = 65.80;
+let altura = 1;
+let peso = 68;
 // cálculo do IMC: peso / altura ao quadrado
 
 
@@ -11,23 +9,25 @@ Math.round(imc);
 if ( imc <= 18.5){
 	alert ( ' Seu IMC é: '+ Math.round(imc) + ';'+
 		'\n Sua classificação é de Magreza!')
-	}else if( imc >= 18.6 && imc <= 24.9999){
+	}else
+	 if( imc >= 18.6 && imc <= 24.9999){
 		alert ( ' Seu IMC é: '+ Math.round(imc)+';'+
 		'\nSua classificação é de Normal!')
-	}else if( imc >= 25 && imc <= 29.99999){
+	}else 
+	if( imc >= 25 && imc <= 29.99999){
 		alert ( ' Seu IMC é: '+ Math.round(imc)+ ';' +
 		'\n Sua classificação é de Sobrepeso!'+
 		'\nGrau de Obesidade: I.')
-	}else if( imc >= 30 && imc <= 39.9999){
+	}else 
+	if( imc >= 30 && imc <= 39.9999){
 		alert ( ' Seu IMC é: '+ Math.round(imc)+ ';'+
 		'\n Sua classificação é de Obesidade!'+
 		'\nGrau de Obesidade: II.')
-	}else if( imc >= 40){
+	}else 
+	if( imc >= 40){
 		alert ( ' Seu IMC é: ' +Math.round(imc)+ ';'+
 		'\n Sua classificação é de Obesidade Grave!'+
 		'\nGrau de Obesidade: III.')
 	}
 
 
-
-</script>
